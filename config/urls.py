25edadf2772206c02aa0bca137125818e344e67d
jamesmichael.lausa <jamesmichael.lausa@g.msuiit.edu.ph>
2025-06-22 20:28:19 +0800

    Feat: Add home view for root URL

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,20 @@
-# config/urls.py (New, Simplified Version)
+# config/urls.py
 
 from django.contrib import admin
 from django.urls import path
-from api.views import EyeDetectionView  # We only need to import this one view
+from django.http import HttpResponse  # <--- IMPORT THIS
+from api.views import EyeDetectionView
+
+# A simple view to handle requests to the root URL
+def home(request):
+    return HttpResponse("LeukoLook API is active.", status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # This is the only API endpoint your application needs now
+    # This is your main API endpoint
     path('api/eye-detect/', EyeDetectionView.as_view(), name='eye_detect'),
+
+    # This new path handles the root URL "/"
+    path('', home, name='home'), # <--- ADD THIS LINE
 ]
